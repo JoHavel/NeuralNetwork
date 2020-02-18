d6d953f6cab5c48d5063cab37c8fcb1f9fa2b006
Jonas Havelka <jonas.havelka@moznabude.cz>
2020-02-18 10:19:04 +0100

    Add preamble to every file

--- src/jsMain/kotlin/mnistDatabase/loadFileJS.kt
+++ src/jsMain/kotlin/mnistDatabase/loadFileJS.kt
@@ -1,3 +1,11 @@
+/*
+ * file:               loadFileJS.kt
+ * original author:    Jonáš Havelka <jonas.havelka@volny.cz>
+ * project:            NeuralNetwork - Kotlin library for NNs
+ * content:            fun loadFile, saveFile, loadFileString
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 package mnistDatabase
 
 actual fun loadFile(file: String): ByteArray {
